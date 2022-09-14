594c3fc05f8c46bedaa09af53fd0e11772e80770
wook <taewook@wooksMac.local>
2022-09-14 09:52:25 +0900

    Initial commit

---
+++ src/main/java/com/wook/awswebservice/AwsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wook.awswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsWebServiceApplication.class, args);
+    }
+
+}
